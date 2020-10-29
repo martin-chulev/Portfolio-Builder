f34ac627903a5fe1e23610997b782275f61b8c6b
Martin Chulev <martin.chulev@abv.bg>
2020-10-29 21:49:55 +0200

    Create project

---
+++ PortfolioBuilder/PortfolioBuilder/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PortfolioBuilder.Models;
+
+namespace PortfolioBuilder.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
